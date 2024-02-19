3a32af602d93c58ef996e64aa1c19df27ff7810c
Sarah Abel <sniebe@gmail.com>
2024-02-19 14:21:03 +0100

    Added resources and python server script

--- src/dmi_fetch.js
+++ src/dmi_fetch.js
@@ -70,5 +70,58 @@ class DMIOpenDataClient {
         const data = await response.json();
         return data;
     }
+
+    static async get_closest_station(client, latitude, longitude, params = []) {     
+        const data = await this.query(client, client.api_name, "collections/station/items", { "limit": 10000, "offset": 0 })
+        const stations = data.features;
+
+        let closest_station = {};
+        let closests_dist = 10000000000.0;
+        
+        
+        for (const station of stations) {
+            const coordinates = station.geometry.coordinates;
+            if (coordinates == undefined || coordinates.length < 2) {
+                console.log("skipping coordinate");
+                continue;
+            }
+            const lon = coordinates[0];
+            const lat = coordinates[1];
+
+            if (lon == undefined || lat == undefined) {
+                console.log("skipping coordinate");
+                continue;
+            }
+
+            const station_params = station.properties.parameterId;
+
+            if (!Helpers.check_subset(station_params, params)) {console.log("not matching parameters"); continue};
+
+            const dist = Helpers.distance(latitude, longitude, lat, lon);
+            if (dist < closests_dist) {
+                closests_dist = dist;
+                closest_station = station;
+            }
+
+        }
+        return closest_station;
+    }
+}
+
+class Helpers{
+
+    static check_subset(parentArray, subsetArray){
+        return subsetArray.every((el) => {
+            return parentArray.includes(el)
+        })
+    }
+    
+    static distance(lat1, lon1, lat2, lon2) {
+        
+        const p = Math.PI / 180.0;
+        const CONST_EARTH_DIAMETER = 12742; // km
+        const a = 0.5 - Math.cos((lat2 - lat1) * p) / 2.0 + Math.cos(lat1 * p) * Math.cos(lat2 * p) * (1.0 - Math.cos((lon2 - lon1) * p)) / 2;
+        return CONST_EARTH_DIAMETER * Math.asin(Math.sqrt(a));  // 2*R*asin...
+    }
 }
 export { DMIOpenDataClient };
